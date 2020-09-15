50a9bd551d59ca7bc5e5c4f86c540b965942b178
michael garrison <mgarrison.sa@gmail.com>
2020-09-14 19:02:51 -0500

    Fizz Buzz

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -5,6 +5,7 @@ public class ControlFlow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+//========LOOPS=============
         int i = 5;
         while(i <= 15) {
             System.out.println(i);
@@ -44,6 +45,24 @@ public class ControlFlow {
         for (long m = 2L; m < 1000000; m *= m) {
             System.out.println(m);
         }
+
+
+//=========FIZZ BUZZ===========
+//        For multiples of three: print “Fizz” instead of the number.
+//        For the multiples of five: print “Buzz”.
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
+        for (int p = 1; p <= 100; p++) {
+            if(p % 15 == 0) {
+                System.out.println("FIzzBuzz");
+            } else if (p % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (p % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(p);
+            }
+        }
+
 
     }
 
