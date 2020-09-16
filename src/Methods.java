f7e210928e3816181568e25e341dcdd869c0a18e
michael garrison <mgarrison.sa@gmail.com>
2020-09-15 21:20:21 -0500

    java factorials

--- src/Methods.java
+++ src/Methods.java
@@ -7,6 +7,26 @@ public class Methods {
 
         int userInt = getInteger(1,10);
         System.out.println(userInt);
+
+        int number;
+        String response;
+        long fact = 1;
+        do {
+            number = getInteger(1, 10);
+            System.out.println("Calculate factorial of " + number + "!");
+            System.out.println("continue?(yes/no)");
+            response = scanner.next();
+        } while (!response.equalsIgnoreCase("Yes"));
+        System.out.println(fact);
+        System.out.print(number + "! = ");
+        for (int i = 1; i <= number; i++) {
+            if (i < number) {
+                System.out.print(i + " x ");
+            } else {
+                System.out.println(i + " = " + factorialRecursion(number));
+            }
+        }
+
     }
 
     static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
@@ -51,6 +71,14 @@ public class Methods {
         }
         System.out.println("You entered a valid number");
         return userInput;
+    }
+
+
+    public static long factorialRecursion(int num) {
+        if (num <= 1) {
+            return 1;
+        }
+        return num * factorialRecursion(num - 1);
     }
 
 
