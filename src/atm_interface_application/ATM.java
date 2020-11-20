eea2968d18794e05ada2da4f7410d47e6977f34b
michael garrison <mgarrison.sa@gmail.com>
2020-11-19 19:07:32 -0600

    continued working on atm class

--- src/atm_interface_application/ATM.java
+++ src/atm_interface_application/ATM.java
@@ -1,4 +1,83 @@
 package atm_interface_application;
 
+import java.util.Scanner;
+
 public class ATM {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Bank theBank = new Bank("Bank");
+        User aUser = theBank.addUser("John", "Dow", "1234");
+        Account newAccount = new Account("Checking", aUser, theBank);
+        aUser.addAccount(newAccount);
+        theBank.addAccount(newAccount);
+
+        User curUser;
+        while (true){
+            curUser = ATM.mainMenuPrompt(theBank, sc);
+            ATM.printUserMenu(curUser, sc);
+        }
+    }
+
+    private static void printUserMenu(User curUser, Scanner sc) {
+
+    }
+
+    private static User mainMenuPrompt(Bank theBank, Scanner sc) {
+        String userID;
+        String pin;
+        User authUser;
+
+        do {
+            System.out.printf("\n\nWelcome to %s\n\n", theBank.getName());
+            System.out.print("Enter user ID: ");
+            userID = sc.nextLine();
+            System.out.print("Enter pin: ");
+            pin = sc.nextLine();
+            authUser = theBank.userLogin(userID, pin);
+            if (authUser == null){
+                System.out.println("incorrect user ID/pin combination. " + "Please try agin");
+
+            }
+        } while(authUser == null);
+        return authUser;
+    }
+
+    public static void printUserMenu(User theUser, Scanner sc){
+        theUser.printAcountSummary();
+        int choice;
+        do {
+            System.out.printf("Welcome %s, what would you like to do?", theUser.getFirstName());
+            System.out.println(" 1) Show account transaction history");
+            System.out.println(" 2) Withdraw");
+            System.out.println(" 3) Deposit");
+            System.out.println(" 4) Transfer");
+            System.out.println(" 5) Quit");
+            System.out.println();
+            System.out.println("Enter choice: ");
+            choice = sc.nextInt();
+
+            if(choice < 1 || choice > 5){
+                System.out.println("invalid choice. Please choose 1-5");
+            }
+        } while (choice < 1 || choice > 5);
+        switch (choice){
+            case 1:
+                ATM.showTransHistory(theUser, sc);
+                break;
+            case 2:
+                ATM.withrdawFunds(theUser, sc);
+                break;
+            case 3:
+                ATM.depositFunds(theUser, sc);
+                break;
+            case 4:
+                ATM.transferFunds(theUser, sc);
+                break;
+        }
+        if(choice != 5){
+            ATM.printUserMenu(theUser, sc);
+        }
+    }
+
 }
