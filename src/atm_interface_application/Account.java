1f76f4144c76ff1d36fe7b518bb86e08bf5acee5
michael garrison <mgarrison.sa@gmail.com>
2020-11-19 19:08:23 -0600

    continued working on account class

--- src/atm_interface_application/Account.java
+++ src/atm_interface_application/Account.java
@@ -12,12 +12,29 @@ public class Account {
     public Account(String name, User holder, Bank theBank){
         this.name = name;
         this.holder = holder;
-        this.uuid = theBank.getNewUserUUID();
+        this.uuid = theBank.getNewAccountUUID();
         this.transactions = new ArrayList<Transaction>();
     }
 
     public String getUUID(){
         return this.uuid;
+    }
+
+    public String getSummaryLine(){
+        double balance = this.getBalance();
+        if(balance >= 0){
+            return String.format("%s : $%.02f : %s", this.uuid, balance, this.name);
+        } else {
+            return String.format("%s : $(%.02f0 : %s", this.uuid, balance, this.name);
+        }
+    }
+
+    public double getBalance(){
+        double balance = 0;
+        for(Transaction t : this.transactions){
+            balance += t.getAmount();
+        }
+        return balance;
     }
 
 }
