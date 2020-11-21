7d77d6d72fb743f1b77f9ba66fca0166caa59c22
michael garrison <mgarrison.sa@gmail.com>
2020-11-20 20:19:48 -0600

    continued working on atm class

--- src/atm_interface_application/ATM.java
+++ src/atm_interface_application/ATM.java
@@ -44,7 +44,7 @@ public class ATM {
     }
 
     public static void printUserMenu(User theUser, Scanner sc){
-        theUser.printAcountSummary();
+        theUser.printAccountsSummary();
         int choice;
         do {
             System.out.printf("Welcome %s, what would you like to do?", theUser.getFirstName());
@@ -78,6 +78,59 @@ public class ATM {
         if(choice != 5){
             ATM.printUserMenu(theUser, sc);
         }
+    }
+
+    private static void depositFunds(User theUser, Scanner sc) {
+    }
+
+    private static void transferFunds(User theUser, Scanner sc) {
+        int fromAcct;
+        int toAcct;
+        double amount;
+        double acctBal;
+
+        do {
+            System.out.printf("Enter the number (1-%d) of the acount\n" + "to transfer from: ");
+            fromAcct = sc.nextInt()-1;
+            if (fromAcct < 0 || fromAcct >= theUser.numAccounts()){
+                System.out.println("Invalid account. Please try again");
+            }
+        } while (fromAcct < 0 || fromAcct >= theUser.numAccounts());
+        acctBal = theUser.getAcctBalance(fromAcct);
+        do {
+            System.out.printf("Enter the number (1-%d) of the account\n" + "to transfer to: ");
+            toAcct = sc.nextInt()-1;
+            if(toAcct < 0 || toAcct >= theUser.numAccounts()){
+                System.out.println("Invalid account. Please try again.");
+            }
+        } while (toAcct < 0 || toAcct >= theUser.numAccounts());
+        do {
+            System.out.printf("Enter the amount to transfer (max $%.02f): $", acctBal);
+            amount = sc.nextDouble();
+            if(amount < 0){
+                System.out.println("Amount must be greater than zero.");
+            } else if (amount > acctBal){
+                System.out.printf("Amount must not be greater than \n" + "balance of $0.02f.\n", acctBal);
+            }
+        } while (amount < 0 || amount > acctBal);
+        theUser.addAcctTransaction(fromAcct, -1*amount, String.format("Transfer to account %s", theUser.getAcctUUID(toAcct)));
+        theUser.addAcctTransaction(toAcct, amount, String.format("Transfer to account %s", theUser.getAcctUUID(fromAcct)));
+
+    }
+
+    private static void withrdawFunds(User theUser, Scanner sc) {
+    }
+
+    public static void showTransHistory(User theUser, Scanner sc){
+        int theAcct;
+        do{
+            System.out.printf("Enter the number (1-%d of the account" + " whose transactions you want to see: ", theUser.numAccounts());
+            theAcct = sc.nextInt()-1;
+            if(theAcct < 0 || theAcct >= theUser.numAccounts()){
+                System.out.println("Invalid account. Please try again");
+            }
+        } while (theAcct < 0 || theAcct >= theUser.numAccounts());
+        theUser.printAcctTransHistory(theAcct);
     }
 
 }
