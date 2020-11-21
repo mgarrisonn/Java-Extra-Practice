39c413f177ae76565cbf3cfd366e3bc5a15ed2cb
michael garrison <mgarrison.sa@gmail.com>
2020-11-20 20:20:28 -0600

    continued working on transaction class

--- src/atm_interface_application/Transaction.java
+++ src/atm_interface_application/Transaction.java
@@ -26,5 +26,12 @@ public class Transaction {
     }
 
 
+    public String getSummaryLine() {
+        if(this.amount >= 0){
+            return String.format("%s : $%.02f : %s", this.timestamp.toString(), this.amount, this.memo);
+        } else {
+            return String.format("%s : $(%.02f) : %s", this.amount, this.memo);
 
+        }
+    }
 }
