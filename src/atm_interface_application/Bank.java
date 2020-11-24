44816fdeab65204343412fa085ccfa057bcbc004
michael garrison <mgarrison.sa@gmail.com>
2020-11-23 23:01:46 -0600

    completed bank class

--- src/atm_interface_application/Bank.java
+++ src/atm_interface_application/Bank.java
@@ -19,6 +19,28 @@ public class Bank {
     public String getNewUserUUID(){
         String uuid;
         Random rng = new Random();
+        int len = 6;
+        boolean nonUnique;
+        do {
+            uuid = "";
+            for (int c = 0; c < len; c++){
+                uuid += ((Integer)rng.nextInt(10)).toString();
+            }
+            nonUnique = false;
+            for (User u : this.users){
+                if(uuid.compareTo(u.getUUID()) == 0){
+                    nonUnique = true;
+                    break;
+                }
+            }
+        } while(nonUnique);
+        return uuid;
+    }
+
+    
+    public String getNewAccountUUID(){
+        String uuid;
+        Random rng = new Random();
         int len = 10;
         boolean nonUnique;
         do {
@@ -35,14 +57,6 @@ public class Bank {
             }
         } while(nonUnique);
         return uuid;
-    }
-
-    public String getNewUserUUID(){
-
-    }
-    
-    public String getNewAccountUUID(){
-        
     }
 
     public void addAccount(Account anAcct) {
