23cd0f21d486d9595c40ceb94b4453161f93928b
michael garrison <mgarrison.sa@gmail.com>
2020-11-23 23:00:30 -0600

    completed atm class

--- src/atm_interface_application/ATM.java
+++ src/atm_interface_application/ATM.java
@@ -1,10 +1,11 @@
 package atm_interface_application;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class ATM {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         Scanner sc = new Scanner(System.in);
         Bank theBank = new Bank("Bank");
         User aUser = theBank.addUser("John", "Dow", "1234");
@@ -19,11 +20,8 @@ public class ATM {
         }
     }
 
-    private static void printUserMenu(User curUser, Scanner sc) {
 
-    }
-
-    private static User mainMenuPrompt(Bank theBank, Scanner sc) {
+    private static User mainMenuPrompt(Bank theBank, Scanner sc) throws NoSuchAlgorithmException {
         String userID;
         String pin;
         User authUser;
@@ -47,7 +45,7 @@ public class ATM {
         theUser.printAccountsSummary();
         int choice;
         do {
-            System.out.printf("Welcome %s, what would you like to do?", theUser.getFirstName());
+            System.out.printf("Welcome %s, what would you like to do?\n", theUser.getFirstName());
             System.out.println(" 1) Show account transaction history");
             System.out.println(" 2) Withdraw");
             System.out.println(" 3) Deposit");
@@ -74,6 +72,9 @@ public class ATM {
             case 4:
                 ATM.transferFunds(theUser, sc);
                 break;
+            case 5:
+                sc.nextLine();
+                break;
         }
         if(choice != 5){
             ATM.printUserMenu(theUser, sc);
@@ -81,6 +82,31 @@ public class ATM {
     }
 
     private static void depositFunds(User theUser, Scanner sc) {
+        int toAcct;
+        double amount;
+        double acctBal;
+        String memo;
+
+        do {
+            System.out.printf("Enter the number (1-%d) of the acount\n" + "to deposit in: ", theUser.numAccounts());
+            toAcct = sc.nextInt()-1;
+            if (toAcct < 0 || toAcct >= theUser.numAccounts()){
+                System.out.println("Invalid account. Please try again");
+            }
+        } while (toAcct < 0 || toAcct >= theUser.numAccounts());
+        acctBal = theUser.getAcctBalance(toAcct);
+        do {
+            System.out.printf("Enter the amount to transfer (max $%.02f): $", acctBal);
+            amount = sc.nextDouble();
+            if(amount < 0){
+                System.out.println("Amount must be greater than zero.");
+            }
+        } while (amount < 0);
+
+        sc.nextLine();
+        System.out.print("Enter a memo: ");
+        memo = sc.nextLine();
+        theUser.addAcctTransaction(toAcct, amount, memo);
     }
 
     private static void transferFunds(User theUser, Scanner sc) {
@@ -90,7 +116,7 @@ public class ATM {
         double acctBal;
 
         do {
-            System.out.printf("Enter the number (1-%d) of the acount\n" + "to transfer from: ");
+            System.out.printf("Enter the number (1-%d) of the acount\n" + "to transfer from: ", theUser.numAccounts());
             fromAcct = sc.nextInt()-1;
             if (fromAcct < 0 || fromAcct >= theUser.numAccounts()){
                 System.out.println("Invalid account. Please try again");
@@ -98,7 +124,7 @@ public class ATM {
         } while (fromAcct < 0 || fromAcct >= theUser.numAccounts());
         acctBal = theUser.getAcctBalance(fromAcct);
         do {
-            System.out.printf("Enter the number (1-%d) of the account\n" + "to transfer to: ");
+            System.out.printf("Enter the number (1-%d) of the account\n" + "to transfer to: ", theUser.numAccounts());
             toAcct = sc.nextInt()-1;
             if(toAcct < 0 || toAcct >= theUser.numAccounts()){
                 System.out.println("Invalid account. Please try again.");
@@ -119,6 +145,33 @@ public class ATM {
     }
 
     private static void withrdawFunds(User theUser, Scanner sc) {
+        int fromAcct;
+        double amount;
+        double acctBal;
+        String memo;
+
+        do {
+            System.out.printf("Enter the number (1-%d) of the account\n" + "to withdraw from: ", theUser.numAccounts());
+            fromAcct = sc.nextInt()-1;
+            if (fromAcct < 0 || fromAcct >= theUser.numAccounts()){
+                System.out.println("Invalid account. Please try again");
+            }
+        } while (fromAcct < 0 || fromAcct >= theUser.numAccounts());
+        acctBal = theUser.getAcctBalance(fromAcct);
+        do {
+            System.out.printf("Enter the amount to withdraw (max $%.02f): $", acctBal);
+            amount = sc.nextDouble();
+            if(amount < 0){
+                System.out.println("Amount must be greater than zero.");
+            } else if (amount > acctBal){
+                System.out.printf("Amount must not be greater than \n" + "balance of $0.02f.\n", acctBal);
+            }
+        } while (amount < 0 || amount > acctBal);
+
+        sc.nextLine();
+        System.out.print("Enter a memo: ");
+        memo = sc.nextLine();
+        theUser.addAcctTransaction(fromAcct, -1*amount, memo);
     }
 
     public static void showTransHistory(User theUser, Scanner sc){
