e0bb0e8eaa222f58fb1d912e2588863ba07e3306
michael garrison <mgarrison.sa@gmail.com>
2020-12-01 19:00:54 -0600

    set up user interface

--- src/adventure_game/Main.java
+++ src/adventure_game/Main.java
@@ -8,6 +8,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
+        String[] enemies = {"Skeleton", "Zombie", "Warrior", "Assassin"};
+
+        int maxEnemyHealth = 100;
+        int enemyAttackDamage = 25;
+        int health = 100;
+        int attackDamage = 50;
+        int numHealthPotions = 3;
+        int healthPotionHealAmount = 30;
+        int healthPotionDropChance = 50;
+
+        boolean running = true;
+
+        System.out.println("Welcome to the Dungeon");
+
+        GAME:
+        while(running){
+            System.out.println("--------------------------------------------------------");
+            int enemyHealth = random.nextInt(maxEnemyHealth);
+            String enemy = enemies[random.nextInt(enemies.length)];
+            System.out.println("\t# " + enemy + " has appeared! #\n");
+
+            while(enemyHealth > 0){
+                System.out.println("\tYour HP: " + health);
+                System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
+                System.out.println("\n\tWhat would you like to do?");
+                System.out.println("\t1. Attack");
+                System.out.println("\t2. Drink health potion");
+                System.out.println("\t3. Run!");
+
+                String input = sc.nextLine();
+                if(input.equals("1")){
+
+                }else if(input.equals("2")){
+
+                }else if(input.equals("3")){
+
+                }else {
+
+                }
+            }
+
+        }
 
     }
 }
