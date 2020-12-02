65351d7902b068393a6ce2ff5215381ea2c7a5dd
michael garrison <mgarrison.sa@gmail.com>
2020-12-01 18:44:18 -0600

    created main class for java adventure game

---
+++ src/adventure_game/Main.java
@@ -0,0 +1,13 @@
+package adventure_game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+
+    }
+}
